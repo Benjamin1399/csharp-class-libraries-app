09f502236a0222f494a3784dca8f408dcd17f388
BENJAMIN NAIDOO <benjamin.naidoo@apex-rts.co.za>
2026-08-25 19:24:33 +0200

    Added properties to PersonModel

--- PersonLibrary/Models/PersonModel.cs
+++ PersonLibrary/Models/PersonModel.cs
@@ -8,5 +8,9 @@ namespace PersonLibrary.Models
 {
     public class PersonModel
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
     }
 }
