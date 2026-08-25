761ef54ca46a122c2f8668480427aaf29da52087
BENJAMIN NAIDOO <benjamin.naidoo@apex-rts.co.za>
2026-08-25 19:23:31 +0200

    Created PersonCalculations and PersonModel

---
+++ PersonLibrary/PersonCalculations.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonLibrary
+{
+    public static class PersonCalculations
+    {
+        public static int CalculateAge(int yearBorn)
+        {
+            int age = 0;
+
+            int currentYear = DateTime.Now.Year;
+
+            age = currentYear - yearBorn;
+
+            return age;
+        }
+    }
+}
