8f196f18c23275e931fd6d22f4bcb9bf1b3a1806
BENJAMIN NAIDOO <benjamin.naidoo@apex-rts.co.za>
2026-08-25 19:35:27 +0200

    Added logic for getting input and calculating age

---
+++ ConsoleUI/ConsoleOperations.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    public static class ConsoleOperations
+    {
+        public static int GetIntInput(string message)
+        {
+            int number = 0;
+            string input = "";
+
+            bool isIntValid = false;
+
+            do
+            {
+                Console.Write(message);
+                input = Console.ReadLine();
+
+                isIntValid = int.TryParse(input, out number);
+
+            } while (isIntValid == false);
+
+            return number;
+        }
+
+        public static string GetStringInput(string message)
+        {
+            string text = "";
+
+            Console.Write(message);
+            text = Console.ReadLine();
+
+            return text;
+
+        }
+    }
+}
--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using PersonLibrary;
+using PersonLibrary.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +12,22 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            PersonModel person = new PersonModel();
+
+            string firstName = ConsoleOperations.GetStringInput("Enter first name: ");
+            string lastName = ConsoleOperations.GetStringInput("Enter last name: ");
+
+            int yearBorn = ConsoleOperations.GetIntInput("Enter your birth year: ");
+
+            int age = PersonCalculations.CalculateAge(yearBorn);
+
+            person.FirstName = firstName;
+            person.LastName = lastName;
+            person.Age = age;
+
+            Console.WriteLine($"You are {person.FirstName} {person.LastName} and you are {person.Age} years old.");
+
+            Console.ReadLine();
         }
     }
 }
